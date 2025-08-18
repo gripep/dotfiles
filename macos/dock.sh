527950fece5d753c94cf0cae5c97a59c51e4ad66
Gian <30044863+gripep@users.noreply.github.com>
2025-08-18 11:28:38 +0100

    Update macos dock defaults

--- macos/dock.sh
+++ macos/dock.sh
@@ -4,6 +4,11 @@
 
 echo "Setting up the macOS Dock..."
 
+if ! is-executable dockutil; then
+    echo "Command 'dockutil' not installed. Installing dockutil..."
+    brew install dockutil
+fi
+
 dockutil --no-restart --remove all
 
 dockutil --no-restart --add "/Applications/Google Chrome.app"
@@ -14,8 +19,10 @@ dockutil --no-restart --add "/Applications/Discord.app"
 dockutil --no-restart --add "/Applications/Notion.app"
 dockutil --no-restart --add "/Applications/Todoist.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
-dockutil --no-restart --add "/Applications/System Preferences.app"
+dockutil --no-restart --add "/System/Applications/System Settings.app"
 dockutil --no-restart --add "/Applications/ExpressVPN.app"
+
+dockutil --no-restart --add "/Users/$USER/Downloads" --view list
 
 killall Dock
 

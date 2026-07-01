eb2b0cd3844c75c83964e2bf8015bad118f30fa5
Gian <30044863+gripep@users.noreply.github.com>
2026-07-01 11:34:13 +0100

    refactor: Wrap gemini cli install in func

--- install/tools.sh
+++ install/tools.sh
@@ -4,20 +4,15 @@
 # These tools are better installed via their own installation methods because of specific configuration needs
 # or dependencies clashes with Homebrew packages.
 
-# gemini-cli (Google Gemini AI models cli tool)
-# TODO: check if this is needed
-# export NVM_DIR="$HOME/.nvm"
-if ! command -v nvm &> /dev/null; then
-    # Check if nvm was installed, if not install it via Homebrew
-    echo "nvm not found. Installing nvm via Homebrew..."
-    brew install nvm
-fi
-if ! nvm use stable &> /dev/null; then
-    # Check if nvm use stable works, otherwise install stable
-    # The version stable is the latest stable version of Node.js
-    echo "No stable Node.js version found. Installing stable..."
-    nvm install stable
-fi
-nvm use stable
-# Install gemini-cli globally
-npm install -g @google/gemini-cli
+install_gemini_cli() {
+    # gemini-cli (Google Gemini AI models cli tool)
+    if command -v gemini &> /dev/null; then
+        return
+    fi
+    echo "gemini-cli not found. Installing..."
+    export NVM_DIR="$HOME/.nvm"
+    nvm use stable
+    npm install -g @google/gemini-cli
+}
+
+install_gemini_cli

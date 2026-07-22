927bbd46cfc4cae7f77f411d9b9fba2356298d98
Gian <30044863+gripep@users.noreply.github.com>
2026-07-22 09:48:51 +0100

    fix(install): harden installer for fresh and repeat runs (#1)

--- install/oh-my-zsh.sh
+++ install/oh-my-zsh.sh
@@ -1,36 +1,35 @@
-#!/urs/bin/env bash
+#!/usr/bin/env bash
 
 # This script installs Oh My Zsh, and its plugins and themes
 
 ZSH_DIR="$HOME/.config/zsh"
+OMZ_DIR="$ZSH_DIR/.oh-my-zsh"
 
-# Install Oh My Zsh
-echo "Installing Oh My Zsh..."
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-# Move the Oh My Zsh installation to the correct directory
-# The .zshrc will make sure to point to the right place and not the default $HOME/.oh-my-zsh
-mkdir -p $ZSH_DIR
-mv $HOME/.oh-my-zsh $ZSH_DIR/.oh-my-zsh
-# Any previous .zshrc will be renamed to .zshrc.pre-oh-my-zsh
-if test -f .zshrc.pre-oh-my-zsh; then
-    echo "Updating existing .zshrc file..."
-    rm .zshrc
-    mv .zshrc.pre-oh-my-zsh .zshrc
+# Install Oh My Zsh (skip if already installed)
+if [ ! -d "$OMZ_DIR" ]; then
+    echo "Installing Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    # Move the Oh My Zsh installation to the correct directory
+    # The .zshrc points here instead of the default $HOME/.oh-my-zsh
+    mkdir -p "$ZSH_DIR"
+    mv "$HOME/.oh-my-zsh" "$OMZ_DIR"
+else
+    echo "Oh My Zsh already installed. Continue..."
 fi
 
+# Custom dir to store plugins and themes in the right place
+PLUGINS_DIR="$OMZ_DIR/custom/plugins"
+THEMES_DIR="$OMZ_DIR/custom/themes"
 
-# Set ZSH custom dir to store plugins and themes in the right place
-OMZ_CUSTOM_DIR="$ZSH_DIR/.oh-my-zsh/custom"
-
+# Clone each plugin/theme only if it isn't there already (safe to re-run)
 echo "Installing Oh My Zsh plugins and theme..."
 # Install zsh-autosuggestions plugin
-git clone https://github.com/zsh-users/zsh-autosuggestions ${OMZ_CUSTOM_DIR:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+[ -d "$PLUGINS_DIR/zsh-autosuggestions" ] || git clone https://github.com/zsh-users/zsh-autosuggestions "$PLUGINS_DIR/zsh-autosuggestions"
 # Install zsh-completions plugin
-git clone https://github.com/zsh-users/zsh-completions.git ${OMZ_CUSTOM_DIR:-$HOME/.oh-my-zsh/custom}/plugins/zsh-completions
+[ -d "$PLUGINS_DIR/zsh-completions" ] || git clone https://github.com/zsh-users/zsh-completions.git "$PLUGINS_DIR/zsh-completions"
 # Install zsh-syntax-highlighting plugin
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${OMZ_CUSTOM_DIR:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
+[ -d "$PLUGINS_DIR/zsh-syntax-highlighting" ] || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$PLUGINS_DIR/zsh-syntax-highlighting"
 # Install  Powerlevel10k theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${OMZ_CUSTOM_DIR:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+[ -d "$THEMES_DIR/powerlevel10k" ] || git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$THEMES_DIR/powerlevel10k"
 
 echo "Oh My Zsh and plugins installed successfully."

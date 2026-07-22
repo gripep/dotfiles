8eea474e05a7aff77ef281fc72d2e45769cf8eb1
Gian <30044863+gripep@users.noreply.github.com>
2026-07-22 10:25:58 +0100

    fix(clean): make clean.sh safe and targeted (#4)

--- clean.sh
+++ clean.sh
@@ -4,16 +4,36 @@
 # - be run on a new machine to clean up any personal files and config
 # - be run to clean up a machine before giving it away
 # - be run to clean up a bad install of dotfiles
+#
+# It is destructive. Everything below is removed from your home directory.
 
-# Remove item2 and zsh config
-rm -rf .config/iterm2/themes .config/zsh .oh-my-zsh .zshrc
+set -u
+
+# Always operate on $HOME, regardless of the current working directory
+cd "$HOME" || exit 1
+
+echo "This will permanently remove the following from $HOME:"
+echo "  .config/iterm2, .config/zsh, .oh-my-zsh, .zshrc"
+echo "  .ssh (keys and config), .gitconfig, .gitignore_global"
+echo "  Developer/, Screenshots/"
+printf "Continue? [y/N] "
+read -r reply
+case "$reply" in
+    y | Y) ;;
+    *) echo "Aborted." && exit 0 ;;
+esac
+
+# Remove iterm2 and zsh config
+rm -rf .config/iterm2 .config/zsh .oh-my-zsh .zshrc
 
 # Remove SSH keys and config
-rm -r .ssh/*
-ssh-add -D
+rm -rf .ssh
+ssh-add -D 2>/dev/null || true
 
 # Remove git config
-rm .gitconfig .gitignore_global
+rm -f .gitconfig .gitignore_global
 
 # Remove any personal dirs and files
 rm -rf Developer Screenshots
+
+echo "Cleanup complete."
